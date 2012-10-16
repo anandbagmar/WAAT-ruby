970c8392b628ec213d22576ed57ca0d294f9ca86
Anand Bagmar <abagmar@gmail.com>
2012-10-16 17:58:24 -0300

    Updated test and sample data file for the test

--- features/step_definitions/WAAT-ruby_steps.rb
+++ features/step_definitions/WAAT-ruby_steps.rb
@@ -40,20 +40,22 @@ Given /^I navigate to Anand Bagmar's blog - HttpSniffer$/ do
 end
 
 Given /^I navigate to some page - JsSniffer$/ do
-  url_patterns = ["GET /ps/ifr?container=friendconnect&mid=0"]
-  action_name = "OpenWAATArticleOnBlog_HttpSniffer"
+  base_url = "https://www.mirena.com/en/privacy_statement_user.php"
+
   test_data_file_name = File.join(File.dirname(__FILE__), "..", "..", "sampleData", "TestData.xml")
+  action_name = "OpenAMirenaSecureSite_JsSniffer"
+
+  JS_FOR_MIRENA_SITE = "window.WAAT_URL=\"\";window.WebTrends.prototype.dcsTag=function(){if(document.cookie.indexOf(\"WTLOPTOUT=\")!=-1){return}var WT=this.WT;var DCS=this.DCS;var DCSext=this.DCSext;var i18n=this.i18n;var P=\"http\"+(window.location.protocol.indexOf('https:')==0?'s':'')+\"://\"+this.domain+(this.dcsid==\"\"?'':'/'+this.dcsid)+\"/dcs.gif?\";if(i18n){WT.dep=\"\"}for(var N in DCS){if(DCS[N]&&(typeof DCS[N]!=\"function\")){P+=this.dcsA(N,DCS[N])}}for(N in WT){if(WT[N]&&(typeof WT[N]!=\"function\")){P+=this.dcsA(\"WT.\"+N,WT[N])}}for(N in DCSext){if(DCSext[N]&&(typeof DCSext[N]!=\"function\")){if(i18n){WT.dep=(WT.dep.length==0)?N:(WT.dep+\";\"+N)}P+=this.dcsA(N,DCSext[N])}}if(i18n&&(WT.dep.length>0)){P+=this.dcsA(\"WT.dep\",WT.dep)}if(P.length>2048&&navigator.userAgent.indexOf('MSIE')>=0){P=P.substring(0,2040)+\"&WT.tu=1\"}this.dcsCreateImage(P);window.WAAT_URL=P;this.WT.ad=\"\"};_tag.dcsCustom=function(){}; _tag.DCS.dcscfg=\"1\"; _tag.dcsCollect();return window.WAAT_URL" ;
+
 
   initialize_waat(:waat_plugin => "js_sniffer")
 
-  url = "https://www.mirena.com/en/privacy_statement_user.php"
   @driver = Selenium::WebDriver.for :firefox
-  @driver.get url
+  @driver.get base_url
 
-  JS_FOR_MIRENA_SITE = "window.WAAT_URL=\"\";window.WebTrends.prototype.dcsTag=function(){if(document.cookie.indexOf(\"WTLOPTOUT=\")!=-1){return}var WT=this.WT;var DCS=this.DCS;var DCSext=this.DCSext;var i18n=this.i18n;var P=\"http\"+(window.location.protocol.indexOf('https:')==0?'s':'')+\"://\"+this.domain+(this.dcsid==\"\"?'':'/'+this.dcsid)+\"/dcs.gif?\";if(i18n){WT.dep=\"\"}for(var N in DCS){if(DCS[N]&&(typeof DCS[N]!=\"function\")){P+=this.dcsA(N,DCS[N])}}for(N in WT){if(WT[N]&&(typeof WT[N]!=\"function\")){P+=this.dcsA(\"WT.\"+N,WT[N])}}for(N in DCSext){if(DCSext[N]&&(typeof DCSext[N]!=\"function\")){if(i18n){WT.dep=(WT.dep.length==0)?N:(WT.dep+\";\"+N)}P+=this.dcsA(N,DCSext[N])}}if(i18n&&(WT.dep.length>0)){P+=this.dcsA(\"WT.dep\",WT.dep)}if(P.length>2048&&navigator.userAgent.indexOf('MSIE')>=0){P=P.substring(0,2040)+\"&WT.tu=1\"}this.dcsCreateImage(P);window.WAAT_URL=P;this.WT.ad=\"\"};_tag.dcsCustom=function(){}; _tag.DCS.dcscfg=\"1\"; _tag.dcsCollect();return window.WAAT_URL" ;
-  url = extractUrlFromJavascript(JS_FOR_MIRENA_SITE);
+  extracted_url = extract_url_from_javascript(JS_FOR_MIRENA_SITE);
 
-  params = {:test_data_file_name=> test_data_file_name, :action_name => action_name, :url_patterns => url_patterns, :minimum_number_of_packets => 1}
+  params = {:test_data_file_name=> test_data_file_name, :action_name => action_name, :url_patterns => extracted_url}
   result = verify_web_analytics_data(params)
 
   puts "->#{result.status}"
@@ -64,6 +66,6 @@ Given /^I navigate to some page - JsSniffer$/ do
   end
 end
 
-def extractUrlFromJavascript(javascript)
-  url = @driver.execute_script(javascript)
+def extract_url_from_javascript(javascript)
+  @driver.execute_script(javascript)
 end
